f0a0c99de42b091ec4462934b28db7e0ce231ef1
Simuzar <fatulazade96@gmail.com>
2024-08-13 17:21:43 +0300

    docs: update src file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArray = []
+    if(matrix) {
+      matrix.forEach((arr) => {
+        newArray = newArray.concat(arr);
+        return newArray;
+      })
+    } else {
+      return newArray;
+    }
+
+    return newArray;
 }
